35cda0ef93bf8f2d713b57a7ad97b621781aa91a
YoonHan0 <dbsgksdud12@naver.com>
2024-07-05 00:19:20 +0900

    CORS 해결을 위한 proxy 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/test")
+      .then(res => res.text())
+      .then(m =>setMessage(m))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {`넘어온 데이터 확인`}
+        </p>
+        <p>
+          {message}
         </p>
         <a
           className="App-link"
